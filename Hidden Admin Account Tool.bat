d6880be03961e6b175c9195e1a8fb79bbbedb4d6
programmerBrandon <100105534+programmerBrandon@users.noreply.github.com>
2023-07-24 23:39:15 -0400

    Windows Hidden Admin Account Tool - Version 1.0
    A script to quickly enable or disable the hidden administrator account in Windows. Script must be run as Administrator to work properly, but a message will be displayed prompting user to rerun the script with admin priviledges if hasn't been.

---
+++ Hidden Admin Account Tool.bat
@@ -0,0 +1,57 @@
+@echo off
+
+:: Script Name: Hidden Admin Account Tool (Windows) ::
+:: Author: Brandon Green ::
+:: Date Created: 7/24/2023 ::
+:: Date Last Modified: 7/24/2023 ::
+:: Version: 1.0 ::
+
+echo Hidden Admin Account Tool (Windows) - Version 1.0 - Author: Brandon Green
+echo.
+
+net session >nul 2>&1
+ if [%errorLevel%] == [0] (
+ 	choice /c 12 /m "Type 1 to activate the hidden administrator account, or 2 to deactivate it."
+ ) else (
+ 	echo Error: This script requires admin priviledges. Please rerun the script as administrator.
+	pause
+	exit
+ )
+
+::echo 'Type 1 to activate the hidden account, or 2 to deactivate it.'
+::choice /c 12 /m "Type 1 to activate the hidden administrator account, or 2 to deactivate it."
+
+::if ERRORLEVEL == 2 goto caseTwo :: If '2' was typed ::
+
+:: If '2' was typed ::
+if ERRORLEVEL == 2 (
+	goto :caseTwo
+)
+
+:: If '1' was typed ::
+if ERRORLEVEL == 1 (
+	goto caseOne
+)
+
+
+:caseOne
+echo.
+net user administrator /active:yes
+echo Hidden Administrator account has been activated!
+pause
+goto END
+
+:caseTwo 
+echo.
+net user administrator /active:no
+echo Hidden Administrator account has been deactivated!
+pause
+goto END
+
+:END
+exit
+
+::echo Press any key to exit! 
+
+
+
