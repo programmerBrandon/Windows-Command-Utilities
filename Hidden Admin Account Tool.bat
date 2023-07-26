593a9a50f94942cc9243c2da0482d593ca114a0a
programmerBrandon <100105534+programmerBrandon@users.noreply.github.com>
2023-07-25 23:14:47 -0400

    Update Hidden Admin Account Tool.bat
    Added comments to sections that needed it, also made it so the actual command is displayed to the user instead of being hidden.

--- Hidden Admin Account Tool.bat
+++ Hidden Admin Account Tool.bat
@@ -3,12 +3,13 @@
 :: Script Name: Hidden Admin Account Tool (Windows) ::
 :: Author: Brandon Green ::
 :: Date Created: 7/24/2023 ::
-:: Date Last Modified: 7/24/2023 ::
+:: Date Last Modified: 7/25/2023 ::
 :: Version: 1.0 ::
 
 echo Hidden Admin Account Tool (Windows) - Version 1.0 - Author: Brandon Green
 echo.
 
+:: Check that script is run as Administrator ::
 net session >nul 2>&1
  if [%errorLevel%] == [0] (
  	choice /c 12 /m "Type 1 to activate the hidden administrator account, or 2 to deactivate it."
@@ -18,33 +19,41 @@ net session >nul 2>&1
 	exit
  )
 
-:: If '2' was typed ::
+:: If '2' was typed (disable hidden admin account) ::
 if ERRORLEVEL == 2 (
 	goto :caseTwo
 )
 
-:: If '1' was typed ::
+:: If '1' was typed (enable hidden admin account) ::
 if ERRORLEVEL == 1 (
 	goto caseOne
 )
 
-
+:: Enable hidden admin account ::
 :caseOne
 echo.
+echo net user administrator /active:yes
+echo.
+
+:: COMMAND ::
 net user administrator /active:yes
+
 echo Hidden Administrator account has been activated!
 pause
 goto END
 
+:: Disable hidden admin account ::
 :caseTwo 
 echo.
+echo net user administrator /active:no
+echo.
+
+:: COMMAND ::
 net user administrator /active:no
+
 echo Hidden Administrator account has been deactivated!
 pause
 goto END
 
 :END
 exit
-
-
-
